61a918e7aad4f7ec8d76f6622310580b7a4794b0
rocketpowers <rocketpowerswillian@gmail.com>
2021-10-27 11:19:59 -0300

    jpa-class

---
+++ src/aplication/Program.java
@@ -0,0 +1,20 @@
+package aplication;
+
+import domain.People;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		People p1= new People(1,"carlos silva", "carlos@gmail.com");
+		People p2= new People(1,"maria clara", "maria@gmail.com");
+		People p3= new People(1,"vitoria regia", "vitoria@gmail.com");
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+
+	}
+
+}
